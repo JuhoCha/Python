5dba25217323febf34647f1ac72392201fda4b6e
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-06-24 16:28:00 +0900

    Create 변수 설정과 불린변수.py

---
+++ 변수 설정과 불린변수.py
@@ -0,0 +1,9 @@
+character_name="John" 
+character_age="35"
+is_male = False
+print("There once was a man named")
+print("There once was a man named" + character_name + "," + character_age)
+
+character_name="Mike"
+print("There once was a man named")
+print("There once was a man named" + character_name + "," + character_age)
