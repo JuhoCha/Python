ea0e3d81084346d47734c2b899d21cfdf979b772
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-06-24 17:30:20 +0900

    Create 숫자 배열 관련.py

---
+++ 숫자 배열 관련.py
@@ -0,0 +1,30 @@
+print(2)
+print(2.0978)
+print(-2.987)
+print(3+4.5)
+print(3*4+5)
+print(10%3)
+#이건 모듈러라고 하는데 10을 3으로 나눠서 9로 나눌 수 있고 나머지 1이니까 1이 나오는 나머지를 구하는거임
+my_num = -5
+print(str(my_num)+ "my favorite number")
+#str 변수 써야 하는데 
+print(abs(my_num))
+#절댓값
+print(pow(3, 2))
+#제곱해주는거임
+print(max(4, 6))
+#제일 큰거 찾아줌
+print(min(1, 10))
+#작은거
+print(round(3.5))
+#반올림 해줌
+
+from math import*
+#이거 해주면 더 많은 수학 기호에 접근할 수 있음!
+my_num = -6
+print(floor(3.7))
+#작은수
+print(ceil(3.7))
+#무조건 올림
+print(sqrt(36))
+#루트
