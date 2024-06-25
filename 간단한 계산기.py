617f5ca384d95eb9b0c9f558298422acc7de7f35
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-06-25 13:14:06 +0900

    Create 간단한 계산기.py

---
+++ 간단한 계산기.py
@@ -0,0 +1,7 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+
+print(result)
+#이렇게 하면 똑바로 숫자가 안나오는데 우리가 뭘 입력하던 줄과 줄 코드로 인식해서 합쳐주기 위한 변수 사용해야 하는데 int()해주는거임 상수표시 해주면 된당
+#소수도 하고싶으면 float
