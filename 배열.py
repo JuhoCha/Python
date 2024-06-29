00c3692d9acf03ee53dc87025ff69c472cf1a064
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-06-29 15:11:18 +0900

    Update 배열.py

--- 배열.py
+++ 배열.py
@@ -5,3 +5,11 @@ print(friends1[-3]) #두개 같이는 안되는듯 그리고 -1하면 맨 끝이
 friends2 = ["Kevin", "Karen", "Jim"]
                # 0      1    2
 print(friends2[0])
+
+#배열 배우기
+friends3 = ["Kevin", "Karen", "Jim"]
+print(friends3[1:3]) #두개 같이 하려면 : 콜른으로 엮어줘야 하고
+friends3[1] = "Mike"
+print(friends3) #정의 바꿔버리면 바뀐채로 나옴
+
+
