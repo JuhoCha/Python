736f24e2b97972962792786777f3b7434d1a1ee4
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-06-29 15:08:42 +0900

    Create 배열.py

---
+++ 배열.py
@@ -0,0 +1,7 @@
+#배열 배우기
+friends1 = ["Kevin", "Karen", "Jim"]
+print(friends1[-3]) #두개 같이는 안되는듯 그리고 -1하면 맨 끝이되고 -2하면 중간 -3 하면 맨 앞게 된다 
+
+friends2 = ["Kevin", "Karen", "Jim"]
+               # 0      1    2
+print(friends2[0])
