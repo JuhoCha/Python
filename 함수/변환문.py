68b27a113c2c055d1dfe595393c874ecb64e087d
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-06-29 17:15:45 +0900

    Create 변환문.py

---
+++ 함수/변환문.py
@@ -0,0 +1,11 @@
+def cube(num):
+    return num*num*num
+
+
+print(cube(3))#이렇게하면 NONE이라고 나오는데 return 해줘야함
+
+def cube1(num):
+    return num*num*num
+
+result = cube1(4)
+print(result)#이렇게하면 NONE이라고 나오는데 return 해줘야함
